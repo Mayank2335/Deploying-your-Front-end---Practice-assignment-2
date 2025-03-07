bb864f7c8cfb82d01beb01172a1f8397d07af13c
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-03-07 11:19:56 +0530

    Deployed Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Hello, I’m Mayank Sharma</h1>
+      <p>Contact: Email- sharmayank2335@gmail.com <br />Mobile no.-971878xxxx</p>
+    </div>
+  );
+}
+
+export default App;
